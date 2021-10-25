f7e793fd091346d3193984afaaf3a95f8646808f
Henrique <henriquefernandes.gouveia@gmail.com>
2021-10-25 00:17:38 -0300

    criando o footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+
+    return(
+        <>
+          
+          <footer id="footer">
+              <p>Desenvolvido por - <a target="_blank" href="https://www.linkedin.com/in/henrique-fernandes-a29057214/">Henrique Fernandes</a></p>
+          </footer>
+
+        </>
+    );
+}
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./Header.css";
 import  Logo from "../../assets/logo.png";
-import Linkedin from "../../assets/linkedin.png";
 
 export default function Header(props) {
 
@@ -16,9 +15,6 @@ export default function Header(props) {
                   <li><a href="#">Certificação de adoção</a></li>
               </ul>
           </header>
-          <footer id="footer">
-              <p>Desenvolvido por - <a target="_blank" href="https://www.linkedin.com/in/henrique-fernandes-a29057214/">Henrique Fernandes</a></p>
-          </footer>
         </>
     );
 }
